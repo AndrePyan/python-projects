213fd5e7962df2f0f144695af0ce31bf1bd31753
= <=>
2020-04-12 16:06:20 -0500

    Commit

---
+++ index1.py
@@ -0,0 +1,11 @@
+num1 = int(input("Dijite un numero: "))
+num2 = int(input("Otra vez: "))
+resta = num1 - num2
+suma = num1 + num2
+
+pregunta = int(input("PARA RESTA ESCRIBA 1 Y PARA SUMA 2 "))
+
+if pregunta == 1:
+    print(resta)
+if pregunta == 2:
+    print(suma)
---
+++ new.py
@@ -0,0 +1,6 @@
+import msvcrt
+a = 2
+b = 2
+print(a * b)
+msvcrt.getch()
+
